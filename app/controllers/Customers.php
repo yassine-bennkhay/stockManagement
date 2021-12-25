53f0687587505b02288b022021d80ee1713fe9e3
Yassin BENNKHAY <60018749+yassine-bennkhay@users.noreply.github.com>
2021-12-25 20:04:18 +0000

    delete,update customer

--- app/controllers/Customers.php
+++ app/controllers/Customers.php
@@ -73,4 +73,76 @@ class Customers extends Controller
       $this->view('pages/add_customer', $data);
     }
   }
+
+
+
+  public function editCustomer($id)
+  {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      //Sanitize Customer Array=remove any illegal character
+
+      $POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+      $data = [
+        'client_id'=>$id,
+        'name' => trim($_POST['name']),
+        'phone' => trim($_POST['phone']),
+        'address' => trim($_POST['address']),
+        'id'=>trim($_SESSION['user_id']),
+        //errors variables
+        'name_err' => '',
+        'phone_err' => '',
+        'address_err' => ''
+      ];
+
+      //Validate name
+      if (empty($data['name'])) {
+        $data['name_err'] = 'Please enter name';
+      }
+      //Validate phone
+      if (empty($data['phone'])) {
+        $data['phone_err'] = 'Please enter phone number';
+      }
+      //Validate address
+      if (empty($data['address'])) {
+        $data['address_err'] = 'Please enter address';
+      }
+      //Make sure there are no errors
+
+      if (empty($data['name_err']) && empty($data['phone_err']) && empty($data['address_err'])) {
+        //Validated
+        if ($this->clientModel->editACustomer($data)) {
+          flash('customer_message', 'The customer has been edited Successfully!');
+          redirect('/customers/customers');
+        } else {
+          die('Something went wrong');
+        }
+      } else {
+        //Load the view with errors
+        $this->view('pages/edit_customer', $data);
+      }
+    } else {
+      $customer=$this->clientModel->getCustomerById($id);
+      $data = [
+        'client_id'=>$id,
+        'name' =>  $customer->name,
+        'phone' =>  $customer->phone,
+        'address' =>  $customer->address,
+      ];
+      $this->view('pages/edit_customer', $data);
+    }
+  }
+
+
+  public function deleteCustomer($id){
+    if($_SERVER['REQUEST_METHOD']=='POST'){
+    if($this->clientModel->deleteACustomer($id)){
+      flash('delete_message','Customer has been removed successfully!');
+      redirect('/customers/customers');
+    }else{
+      die('Something went wrong!');
+    }
+    }else{
+      redirect('/suppliers/suppliers');
+    }
+      }
 }
--- app/models/Client.php
+++ app/models/Client.php
@@ -1,30 +1,75 @@
 <?php
 
-class Client{
-
-private $db;
-public function __construct()
+class Client
 {
-    $this->db=new Database;
-}
-public function getClient(){
-    $this->db->query('SELECT *FROM clients');
-    $results=$this->db->resultSet();
-    return $results;
-}
 
-public function addACustomer($data){
-    $this->db->query('INSERT INTO clients(name,phone,address) VALUES (:name,:phone,:address)');
-    //Bind values
-    $this->db->bind(':name', $data['name']);
-    $this->db->bind(':phone', $data['phone']);
-    $this->db->bind(':address', $data['address']);
-    //execute
-    if ($this->db->execute()) {
-        return true;
-    } else {
-        return false;
+    private $db;
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
+    public function getClient()
+    {
+        $this->db->query('SELECT *FROM clients');
+        $results = $this->db->resultSet();
+        return $results;
+    }
+
+    public function addACustomer($data)
+    {
+        $this->db->query('INSERT INTO clients(name,phone,address) VALUES (:name,:phone,:address)');
+        //Bind values
+        $this->db->bind(':name', $data['name']);
+        $this->db->bind(':phone', $data['phone']);
+        $this->db->bind(':address', $data['address']);
+        //execute
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    
+    public function editACustomer($data)
+    {
+        $this->db->query('UPDATE clients SET name=:name, phone=:phone,address=:address WHERE client_id=:client_id');
+        //Bind values
+        $this->db->bind(':client_id', $data['client_id']);
+        $this->db->bind(':name', $data['name']);
+        $this->db->bind(':phone', $data['phone']);
+        $this->db->bind(':address', $data['address']);
+       
+        //execute
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+
+
+    public function deleteACustomer($id){
+        $this->db->query('DELETE FROM clients WHERE client_id=:client_id');
+        //Bind values
+        $this->db->bind(':client_id', $id);
+        //execute
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public function getCustomerById($id)
+    {
+        $this->db->query('SELECT * FROM clients WHERE client_id= :id');
+        $this->db->bind(':id', $id);
+        $row = $this->db->single();
+        return $row;
     }
 }
-}
-?>
+
